7a1659e50320dfe584c213a689ca97613f3bc4bf
TanviKokate <75251802+TanviKokate@users.noreply.github.com>
2021-05-05 21:40:37 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,16 @@
+const accessCode1 = "VARIABLE" || "variable";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70);
+    text("A T A D A S B A", 100,100)
+    text("Hint: Stores all Information !!", 100,70);
+    text("R E V B A I L A", 100,150)
+    text("Hint: Performs a particular task !!", 100,70);
+}
